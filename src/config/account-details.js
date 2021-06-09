1c1f3ee0c96f39d69ad86796d25e9fd4b861b431
jschumacher <jschumacher@sajari.com>
2021-06-10 09:24:21 +1000

    Update config file

--- src/config/account-details.js
+++ src/config/account-details.js
@@ -1,7 +1,7 @@
 module.exports = {
-    keyId:"AnqSoFCCoc2bO0vj",
-    keySecret:"pr1t3F9U0NiS5nbe",
-    collectionId: 'commercetools-6',
+    keyId:"",
+    keySecret:"",
+    collectionId: '',
     collectionDisplayName: '',
     defaultEndpoint: "https://api-gateway.sajari.com",
     filename: "./data/example.json"
